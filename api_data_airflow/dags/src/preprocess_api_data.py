2bca09df29ff7dc8a1f6692e5a5811d888df9e7c
Ritika Dhall <ritikapdhall@gmail.com>
2024-11-04 15:45:05 -0500

    Integrating api-data-airflow

--- api_data_airflow/dags/src/preprocess_api_data.py
+++ api_data_airflow/dags/src/preprocess_api_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import requests
+from datetime import datetime
 
 import logging
 
@@ -13,12 +13,14 @@ def preprocess_api_data(output_paths):
         station_status_df = pd.read_csv(status_file_path)
         station_info_df = pd.read_csv(info_file_path)
         merged_df = pd.merge(station_info_df, station_status_df, on='station_id', how='inner')
+
         cleaned_df = merged_df[['station_id', 'short_name', 'name', 'num_bikes_available', 'num_ebikes_available', 'num_docks_available', 'capacity', 'last_reported']]
         cleaned_df['num_cl_bikes_available'] = cleaned_df['num_bikes_available'] - cleaned_df['num_ebikes_available']
         current_time = datetime.now()
-        formatted_time = current_time.strftime('%Y%m%d_%H%M%S')
-        output_path = f'/opt/airflow/dags/data/data_{formatted_time}.csv'
-        cleaned_df.to_csv(output_path, index=False)
+        formatted_time = current_time.strftime('%Y%m%d_%H%M')
+
+        output_path = f'/opt/airflow/dags/data/api_data_{formatted_time}.csv'
+        #cleaned_df.to_csv(output_path, index=False)
         logging.info(f'Data preprocessing completed. Data saved at: {output_path}')
         return output_path
     

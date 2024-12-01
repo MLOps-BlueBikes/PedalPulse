c849fcad46cceb2a6dab7d21a7dea3c0f5e17058
SKashyap-101 <cheruku.sa@northeastern.edu>
2024-11-30 22:07:57 -0500

    Add files via upload

---
+++ training/app/config.py
@@ -0,0 +1,11 @@
+import os  
+GCP_BUCKET_NAME = "skc-pedalpulse-testing"  
+GCP_DATA_FILE = "data_cleaned.csv"
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # gcpdeploy directory
+MODEL_SAVE_PATH = os.path.join(BASE_DIR, 'models')
+
+LOG_PATH = "logs/training.log"
+
+#import os
+#GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
